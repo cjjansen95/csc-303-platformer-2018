40f6de2fd0e74cc37b527bc9b43f28e6d2e4e5b2
SGF\cj95 <jansen095@live.missouristate.edu>
2018-04-02 17:26:59 -0500

    wednesday's changes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,33 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxRandom;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+		
+		var rng:FlxRandom = new FlxRandom();
+		var randVal:Float = rng.float(0, 640);
+		
+		var thing:FlxSprite = new FlxSprite(randVal, 30);
+		thing.makeGraphic(400, 10, FlxColor.CYAN);
+		add(thing);
+		
+		var thing1:FlxSprite = new FlxSprite(30, 130);
+		add(thing1);
+		
+		var thing2:FlxSprite = new FlxSprite(310, 30);
+		add(thing2);
+		
+		var thing3:FlxSprite = new FlxSprite(30, 301);
+		add(thing3);
+		
+		
 	}
 
 	override public function update(elapsed:Float):Void
