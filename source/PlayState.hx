22f46aa3424b9e72566fa3e75943e8cc7c742a9f
Jansen <cj95@live.missouristate.edu>
2018-04-04 11:06:59 -0500

    Part 1 completed

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,17 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.Flxtext;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+		var helloText:FlxText = new FlxText(50, 50, 0, "hello world", true);
+		add(helloText);
+		
 	}
 
 	override public function update(elapsed:Float):Void
